917c31178af48235f3cb2d214ed02e701ad1571e
lfkhan <lfkhan@uwaterloo.ca>
2024-04-05 02:10:07 -0400

    fixed trie

--- main.cc
+++ main.cc
@@ -9,21 +9,29 @@ int main() {
 
     // string s = "left";
 
-    // Trie trie;
-    // trie.insert(s);
-    // trie.insert("level");
+    // string cmd_list[] = {"left", "right", "down", "clockwise", "counterclockwise", 
+    // "drop", "levelup", "leveldown", "norandom", "random", "sequence", "restart"};
 
+    // Trie trie;
+    // for (int i = 0; i < 12; ++i) trie.insert(cmd_list[i]);
+
+    // cout << "ri - closest match: " << trie.return_closest_match("ri") << endl;
     // cout << "le - closest match: " << trie.return_closest_match("le") << endl;
     // cout << "lef - closest match: " << trie.return_closest_match("lef") << endl;
     // cout << "left - closest match: " << trie.return_closest_match("left") << endl;
     // cout << "lev - closest match: " << trie.return_closest_match("lev") << endl;
+    // cout << "levelu - closest match: " << trie.return_closest_match("levelu") << endl;
+    // cout << "ran - closest match: " << trie.return_closest_match("ran") << endl;
     // cout << "levv - closest match: " << trie.return_closest_match("levv") << endl;
     // cout << "levelup - closest match: " << trie.return_closest_match("levelup") << endl;
     // cout << "lefte - closest match: " << trie.return_closest_match("lefte") << endl;
     // cout << "wfefwewefewf - closest match: " << trie.return_closest_match("wfefew") << endl;
+    // for (int i = 0; i < 12; ++i) {
+    //     cout << cmd_list[i] << " - closest match: " << trie.return_closest_match(cmd_list[i]) << endl;
+    // }
 
     // Game game{{Level1{}, Level2{}}};
-    Game game;
+    // Game game;
 }
 
 //This is purely for testing purposes - nowhere close to an actual harness
